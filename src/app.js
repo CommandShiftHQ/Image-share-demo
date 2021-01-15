ec3cabd4796c84aac5d7da8ce4d3674b38b888ba
Ian King <ian.antking@gmail.com>
2021-01-15 19:12:03 +0000

    add static page

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public'))
 
-app.get('/', (_, res) => res.status(200).send('Hello World!'));
+app.get('*', (_, res) => {
+    res.redirect('/');
+});
 
 module.exports = app;
