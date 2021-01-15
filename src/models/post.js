6f2ef4d1ec192e7cce72f95e771b657f8090c99e
Ian King <ian.antking@gmail.com>
2021-01-15 20:42:05 +0000

    date column is unnessesary

--- src/models/post.js
+++ src/models/post.js
@@ -2,7 +2,6 @@ module.exports = (sequelize, DataTypes) => {
     const schema = {
         title: DataTypes.STRING,
         contentUrl: DataTypes.STRING,
-        date: DataTypes.DATE
     }
 
     const postModel = sequelize.define('Post', schema);
