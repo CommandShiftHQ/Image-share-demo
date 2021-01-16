222ea426dcabe6e920dd60064ee368e489b19977
Ian King <ian.antking@gmail.com>
2021-01-16 16:51:14 +0000

    add basic upload script

--- public/js/submit-form.js
+++ public/js/submit-form.js
@@ -1,4 +1,22 @@
-function submitForm(event) {
+async function submitForm(event) {
     event.preventDefault();
-    console.log("The System Works!");
+
+    const title = document.getElementById('ftitle').value;
+    const file = document.getElementById('ffile').files[0];
+    const comment = document.getElementById('fcomment').value;
+
+    const formData = new FormData();
+
+    formData.append('title', title);
+    formData.append('file', file);
+    formData.append('comment', comment);
+
+    const response = await fetch(`http://${window.location.host}/posts`, {
+        method: 'POST',
+        body: formData
+    })
+
+    const responseBody = await response.json()
+
+    console.log(responseBody);
 }
